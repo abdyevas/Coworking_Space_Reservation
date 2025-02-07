b8822ac2172bd44abc7a869eb6e920beb20246ef
abdyevas <sabina.abdyeva@ufaz.az>
2025-02-07 15:32:12 +0400

    Store db properties to the separate file

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -1,19 +1,33 @@
 package database;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/coworking_space";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "740Balqabaq";
+    private static final String PROPERTIES_FILE = "src/main/resources/db.properties";
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
 
     static {
         try {
             Class.forName("org.postgresql.Driver");
+            
+            Properties properties = new Properties();
+            try (FileInputStream fis = new FileInputStream(PROPERTIES_FILE)) {
+                properties.load(fis);
+                URL = properties.getProperty("db.url");
+                USER = properties.getProperty("db.user");
+                PASSWORD = properties.getProperty("db.password");
+            }
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("PostgreSQL Driver not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load database properties file", e);
         }
     }
 
